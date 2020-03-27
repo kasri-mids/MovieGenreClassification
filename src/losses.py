b05be0022c94c690b455aa353314d2ac9c6101fd
sartaj10 <ssbaveja@es.net>
2020-03-26 22:43:20 -0700

    Add losses.py file

---
+++ src/losses.py
@@ -0,0 +1,124 @@
+#@tf.function
+def macro_f1(y, y_hat, thresh=0.5):
+    """Compute the macro F1-score on a batch of observations (average F1 across labels)
+    
+    Args:
+        y (int32 Tensor): labels array of shape (BATCH_SIZE, N_LABELS)
+        y_hat (float32 Tensor): probability matrix from forward propagation of shape (BATCH_SIZE, N_LABELS)
+        thresh: probability value above which we predict positive
+        
+    Returns:
+        macro_f1 (scalar Tensor): value of macro F1 for the batch
+    """
+    y_pred = tf.cast(tf.greater(y_hat, thresh), tf.float32)
+    tp = tf.cast(tf.math.count_nonzero(y_pred * y, axis=0), tf.float32)
+    fp = tf.cast(tf.math.count_nonzero(y_pred * (1 - y), axis=0), tf.float32)
+    fn = tf.cast(tf.math.count_nonzero((1 - y_pred) * y, axis=0), tf.float32)
+    f1 = 2*tp / (2*tp + fn + fp + 1e-16)
+    macro_f1 = tf.reduce_mean(f1)
+    return macro_f1
+
+#@tf.function
+def micro_f1(y, y_hat, thresh=0.5):
+    """Compute the macro F1-score on a batch of observations (average F1 across labels)
+    
+    Args:
+        y (int32 Tensor): labels array of shape (BATCH_SIZE, N_LABELS)
+        y_hat (float32 Tensor): probability matrix from forward propagation of shape (BATCH_SIZE, N_LABELS)
+        thresh: probability value above which we predict positive
+        
+    Returns:
+        macro_f1 (scalar Tensor): value of macro F1 for the batch
+    """
+    y_pred = tf.cast(tf.greater(y_hat, thresh), tf.float32)
+    tp = tf.cast(tf.math.count_nonzero(y_pred * y), tf.float32)
+    fp = tf.cast(tf.math.count_nonzero(y_pred * (1 - y)), tf.float32)
+    fn = tf.cast(tf.math.count_nonzero((1 - y_pred) * y), tf.float32)
+    f1 = 2*tp / (2*tp + fn + fp + 1e-16)
+    micro_f1 = f1 #tf.reduce_mean(f1)
+    return micro_f1
+
+#@tf.function
+def macro_soft_f1(y, y_hat):
+    """Compute the macro soft F1-score as a cost (average 1 - soft-F1 across all labels).
+    Use probability values instead of binary predictions.
+    
+    Args:
+        y (int32 Tensor): targets array of shape (BATCH_SIZE, N_LABELS)
+        y_hat (float32 Tensor): probability matrix from forward propagation of shape (BATCH_SIZE, N_LABELS)
+        
+    Returns:
+        cost (scalar Tensor): value of the cost function for the batch
+    """
+    y = tf.cast(y, tf.float32)
+    y_hat = tf.cast(y_hat, tf.float32)
+    tp = tf.reduce_sum(y_hat * y, axis=0)
+    fp = tf.reduce_sum(y_hat * (1 - y), axis=0)
+    fn = tf.reduce_sum((1 - y_hat) * y, axis=0)
+    soft_f1 = 2*tp / (2*tp + fn + fp + 1e-16)
+    cost = 1 - soft_f1 # reduce 1 - soft-f1 in order to increase soft-f1
+    macro_cost = tf.reduce_mean(cost) # average on all labels
+    return macro_cost
+
+#@tf.function
+def macro_double_soft_f1(y, y_hat):
+    """Compute the macro soft F1-score as a cost (average 1 - soft-F1 across all labels).
+    Use probability values instead of binary predictions.
+    This version uses the computation of soft-F1 for both positive and negative class for each label.
+    
+    Args:
+        y (int32 Tensor): targets array of shape (BATCH_SIZE, N_LABELS)
+        y_hat (float32 Tensor): probability matrix from forward propagation of shape (BATCH_SIZE, N_LABELS)
+        
+    Returns:
+        cost (scalar Tensor): value of the cost function for the batch
+    """
+    y = tf.cast(y, tf.float32)
+    y_hat = tf.cast(y_hat, tf.float32)
+    tp = tf.reduce_sum(y_hat * y, axis=0)
+    fp = tf.reduce_sum(y_hat * (1 - y), axis=0)
+    fn = tf.reduce_sum((1 - y_hat) * y, axis=0)
+    tn = tf.reduce_sum((1 - y_hat) * (1 - y), axis=0)
+    soft_f1_class1 = 2*tp / (2*tp + fn + fp + 1e-16)
+    soft_f1_class0 = 2*tn / (2*tn + fn + fp + 1e-16)
+    cost_class1 = 1 - soft_f1_class1 # reduce 1 - soft-f1_class1 in order to increase soft-f1 on class 1
+    cost_class0 = 1 - soft_f1_class0 # reduce 1 - soft-f1_class0 in order to increase soft-f1 on class 0
+    cost = 0.5 * (cost_class1 + cost_class0) # take into account both class 1 and class 0
+    macro_cost = tf.reduce_mean(cost) # average on all labels
+    return macro_cost
+
+#@tf.function
+POS_WEIGHT = 10
+def weighted_binary_crossentropy(y, y_hat):
+    """
+    Weighted binary crossentropy between an output tensor 
+    and a target tensor. POS_WEIGHT is used as a multiplier 
+    for the positive targets.
+
+    Combination of the following functions:
+    * keras.losses.binary_crossentropy
+    * keras.backend.tensorflow_backend.binary_crossentropy
+    * tf.nn.weighted_cross_entropy_with_logits
+    """
+    y = tf.cast(y, tf.float32)
+    y_hat = tf.cast(y_hat, tf.float32)
+
+    # transform back to logits
+    _epsilon = keras.backend.tensorflow_backend._to_tensor(keras.backend.tensorflow_backend.epsilon(), y_hat.dtype.base_dtype)
+    y_hat = tf.clip_by_value(y_hat, _epsilon, 1 - _epsilon)
+    y_hat = tf.math.log(y_hat / (1 - y_hat))
+    
+    # compute weighted loss
+    loss = tf.nn.weighted_cross_entropy_with_logits(labels=y,
+                                                    logits=y_hat,
+                                                    pos_weight=POS_WEIGHT)
+    return tf.reduce_mean(loss, axis=-1)
+
+#@tf.function
+def binary_crossentropy(y, y_hat):
+    y = tf.cast(y, tf.float32)
+    y_hat = tf.cast(y_hat, tf.float32)
+    loss = tf.keras.losses.binary_crossentropy(y, 
+                                               y_hat,
+                                               from_logits=True)
+    return tf.reduce_mean(loss, axis=-1)
